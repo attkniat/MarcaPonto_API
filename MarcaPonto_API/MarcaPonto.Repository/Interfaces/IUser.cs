9f636cb7a69d979341b300ec1bfce672a3d3564a
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-22 15:32:26 -0300

    Iterface and User Service creation

---
+++ MarcaPonto_API/MarcaPonto.Repository/Interfaces/IUser.cs
@@ -0,0 +1,16 @@
+﻿using MarcaPonto.Model.Usuários;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MarcaPonto.Repository.Interfaces
+{
+    public  interface IUser
+    {
+        Task<bool> CreateUser(Customer customer);
+        Task<Customer> GetCustomerById(string customerIdGuild);
+        Task<Customer> GetCustomerByCPF(string customerCPF);
+        Task<List<Customer>> GetAllCustomers();
+        Task<Customer> UpdateUser(Customer customer);
+        Task<bool> DeleteUser(string customerIdGuild, string CPF);
+    }
+}
---
+++ MarcaPonto_API/MarcaPonto.Repository/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using MarcaPonto.Model.Usuários;
+using MarcaPonto.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarcaPonto.Repository.Services
+{
+    internal class UserService : IUser
+    {
+        public Task<bool> CreateUser(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUser(string customerIdGuild, string CPF)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Customer>> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerByCPF(string customerCPF)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerById(string customerIdGuild)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> UpdateUser(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
