44b991734babb38c9ee2be87ef84412d13a23928
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-22 15:26:53 -0300

    User controler creaning

---
+++ MarcaPonto_API/MarcaPonto_API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarcaPonto_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public void CreateUser()
+        {
+        }
+    }
+}
